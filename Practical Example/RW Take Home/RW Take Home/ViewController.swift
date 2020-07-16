db146fe83e340bf647b7079394cb764f4c50ca2c
zoha131 <abirhasanzoha@gmail.com>
2020-07-16 16:03:50 +0600

    Add CollectionView to the storyboard and Implement it's layout
    Here I have used Compositional Layout. I choose compositional layout because it very easy when it comes to work with different size classes and device orientation. Also it is very helpful to work with accessory features. I have implemented the text size accessory feature.

--- Practical Example/RW Take Home/RW Take Home/ViewController.swift
+++ Practical Example/RW Take Home/RW Take Home/ViewController.swift
@@ -9,12 +9,78 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var collectionView: UICollectionView!
+  @IBOutlet weak var segmentedControl: UISegmentedControl!
+  
+  var dataSource: UICollectionViewDiffableDataSource<String, Int>?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    collectionView.register(TutorialViewCell.self, forCellWithReuseIdentifier: TutorialViewCell.reuseIdentifier)
+    collectionView.collectionViewLayout = createCompositionalLayout()
+    createDataSource()
+    reloadData()
   }
-
-
+  
+  
+  func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
+    
+    let layout = UICollectionViewCompositionalLayout {  sectionIndex, layoutEnvironment in
+      
+      let cellSize = NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(1),
+        heightDimension: .estimated(150)
+      )
+      
+      let layoutItem = NSCollectionLayoutItem(layoutSize: cellSize)
+      
+      let isWidthRegular = self.traitCollection.horizontalSizeClass  == .regular
+      let isLargeTextOn = self.traitCollection.preferredContentSizeCategory.isAccessibilityCategory
+      let itemCount = (isWidthRegular && !isLargeTextOn) ? 2: 1
+      
+      let layoutGroup = NSCollectionLayoutGroup.horizontal(
+        layoutSize: cellSize,
+        subitem: layoutItem,
+        count: itemCount
+      )
+      layoutGroup.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)
+      layoutGroup.interItemSpacing = .fixed(20)
+      
+      let layoutSection = NSCollectionLayoutSection(group: layoutGroup)
+      layoutSection.interGroupSpacing = 20
+      
+      return layoutSection
+    }
+    
+    return layout
+  }
+  
+  func createDataSource() {
+    dataSource = UICollectionViewDiffableDataSource<String, Int>(collectionView: collectionView) { collectionView, indexPath, app in
+      
+      guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TutorialViewCell.reuseIdentifier, for: indexPath) as? TutorialViewCell else {
+        fatalError("Unable to dequeue")
+      }
+      
+      
+      return cell
+    }
+  }
+  
+  func reloadData() {
+    
+    let sections = ["sections"]
+    
+    var snapshot = NSDiffableDataSourceSnapshot<String, Int>()
+    snapshot.appendSections(sections)
+    
+    for section in sections {
+      snapshot.appendItems([1,2,3,4,5,6,7,8,9], toSection: section)
+    }
+    
+    dataSource?.apply(snapshot)
+  }
+  
 }
 
