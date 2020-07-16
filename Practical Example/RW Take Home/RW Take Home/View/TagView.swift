9eec8862dec9e9120adadb050e2cde5d1bcf6774
zoha131 <abirhasanzoha@gmail.com>
2020-07-17 00:31:50 +0600

    Complete implementation of TagView
    The intrinsic size was painful to implement. Without it i was getting view ambiguity when I tried to use content hugging. Finally implemented the intrinsic size. Before this i didn't know abut the `invalidateIntrinsicContentSize()` method. It was good learning opportunity.

--- Practical Example/RW Take Home/RW Take Home/View/TagView.swift
+++ Practical Example/RW Take Home/RW Take Home/View/TagView.swift
@@ -14,103 +14,67 @@ class TagView: UIView {
   let titleLabel = UILabel()
   let iconImageView = UIImageView()
   
+  var title: String? {
+    didSet {
+      titleLabel.text = title
+    }
+  }
+  
+  var iconImage: UIImage? {
+    didSet {
+      iconImageView.image = iconImage
+    }
+  }
+  
+  private let iconSize: CGFloat = 16
+  private let iconHorizontalMargin: CGFloat = 5
+  private let titleTrailingMargin: CGFloat = 8
+  private let titleVerticalMargin: CGFloat = 3
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     
-    titleLabel.text = "hello world"
-    iconImageView.image = UIImage(systemName: "checkmark.circle")
     iconImageView.tintColor = .white
     
-    addSubview(titleLabel)
-    addSubview(iconImageView)
-    
-    titleLabel.font = .preferredFont(forTextStyle: .caption1)
-    titleLabel.adjustsFontForContentSizeCategory = true
-    titleLabel.textColor = .white
+    titleLabel.setupLabel(withTextStyle: .caption1, textColor: .white)
     
     titleLabel.translatesAutoresizingMaskIntoConstraints = false
     iconImageView.translatesAutoresizingMaskIntoConstraints = false
     self.translatesAutoresizingMaskIntoConstraints = false
     
+    addSubview(titleLabel)
+    addSubview(iconImageView)
+    
     layer.cornerRadius = 6
     clipsToBounds = true
     
     NSLayoutConstraint.activate([
-      
-      iconImageView.heightAnchor.constraint(equalToConstant: 16),
+      iconImageView.heightAnchor.constraint(equalToConstant: iconSize),
       iconImageView.widthAnchor.constraint(equalTo: iconImageView.heightAnchor),
       iconImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-      iconImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5),
+      iconImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: iconHorizontalMargin),
       
-      iconImageView.trailingAnchor.constraint(equalTo: titleLabel.leadingAnchor, constant: -5),
+      iconImageView.trailingAnchor.constraint(equalTo: titleLabel.leadingAnchor, constant: -iconHorizontalMargin),
       
-      titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 3),
-      titleLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -3),
-      titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8)
-    
+      titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: titleVerticalMargin),
+      titleLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -titleVerticalMargin),
+      titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -titleTrailingMargin)
     ])
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    invalidateIntrinsicContentSize()
+  }
+  
+  override var intrinsicContentSize: CGSize {
+    let width = titleLabel.intrinsicContentSize.width + titleTrailingMargin + iconSize + (iconHorizontalMargin * 2)
+    let height = titleLabel.intrinsicContentSize.height + (titleVerticalMargin * 2)
     
-    backgroundColor = .article
-
+    return CGSize(width: width, height: height)
   }
   
   required init?(coder: NSCoder) {
     fatalError("Storyboard not supported")
   }
 }
-
-
-//class TagView: UILabel {
-//
-//  var paddingInset = UIEdgeInsets(top: 5, left: 5, bottom: 15, right: 5)
-//
-//  override init(frame: CGRect) {
-//    super.init(frame: frame)
-//
-//    let imageAttachment = NSTextAttachment()
-//    let image = UIImage(systemName: "checkmark.circle")
-//
-//    imageAttachment.image = image?.withTintColor(.white).resized(to: CGSize(width: 16, height: 16))
-//
-//    let fullString = NSMutableAttributedString()
-//    fullString.append(NSAttributedString(attachment: imageAttachment))
-//    fullString.append(NSAttributedString(string: " 2 hrs, 5 mins"))
-//    attributedText = fullString
-//
-//    backgroundColor = .systemRed
-//
-//
-//    font = .preferredFont(forTextStyle: .caption1)
-//    adjustsFontForContentSizeCategory = true
-//    textColor = .white
-//    layer.cornerRadius = 6
-//    clipsToBounds = true
-//
-//  }
-//
-//
-//  required init?(coder: NSCoder) {
-//    fatalError("Storyboard not supported")
-//  }
-//
-//  override func drawText(in rect: CGRect) {
-//    super.drawText(in: rect.inset(by: paddingInset))
-//  }
-//
-//  override var intrinsicContentSize: CGSize {
-//    get {
-//      var contentSize = super.intrinsicContentSize
-//      contentSize.height += paddingInset.top + paddingInset.bottom
-//      contentSize.width += paddingInset.left + paddingInset.right
-//      return contentSize
-//    }
-//  }
-//}
-//
-//extension UIImage {
-//    func resized(to size: CGSize) -> UIImage {
-//        return UIGraphicsImageRenderer(size: size).image { _ in
-//            draw(in: CGRect(origin: .zero, size: size))
-//        }
-//    }
-//}
