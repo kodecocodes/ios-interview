1327e38b044140f4a649beaf5b0ae26244bd5111
Giuliano Soria <44099099+GiulianoSoria@users.noreply.github.com>
2020-08-17 23:30:55 -0700

    Changed dataSource and cellRegistration of RWItemsVC

--- Practical Example/RayWenderlich/Screens/RWItemsVC.swift
+++ Practical Example/RayWenderlich/Screens/RWItemsVC.swift
@@ -13,18 +13,24 @@ protocol RWItemsVCDelegate: class {
 
 class RWItemsVC: UIViewController {
   
+  enum Section { case main }
+  
   weak var delegate: RWItemsVCDelegate!
   
   var collectionView: UICollectionView!
+  var cellRegistration: UICollectionView.CellRegistration<UICollectionViewListCell, String>!
+  var dataSource: UICollectionViewDiffableDataSource<Section, String>!
   
-  var items: [String] = []
+  var items: [String] = ["Library", "Downloads", "My Tutorials"]
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     configureViewController()
     configureCollectionView()
-    
+    configureCellRegistration()
+    configureDataSource()
+    updateData(with: self.items)
   }
   
   func configureViewController() {
@@ -32,14 +38,10 @@ class RWItemsVC: UIViewController {
     view.backgroundColor = .secondarySystemBackground
     navigationController?.navigationBar.tintColor = UIColor(hue:0.365, saturation:0.527, brightness:0.506, alpha:1)
     title = "raywenderlich.com"
-    
-    items.append("Library")
-    items.append("Downloads")
-    items.append("My Tutorials")
   }
   
   func configureCollectionView() {
-    let configuration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+    let configuration = UICollectionLayoutListConfiguration(appearance: .sidebar)
     let layout = UICollectionViewCompositionalLayout.list(using: configuration)
     collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
     
@@ -48,36 +50,70 @@ class RWItemsVC: UIViewController {
     collectionView.backgroundColor = .secondarySystemBackground
     
     collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "ItemCell")
-    collectionView.dataSource = self
+//    collectionView.dataSource = self
     collectionView.delegate = self
+  }
+  
+  func configureCellRegistration() {
+    cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, String>(handler: { cell, indexPath, name in
+      var configuration = UIListContentConfiguration.cell()
+      
+      configuration.text = name
+      configuration.imageProperties.tintColor = UIColor(hue:0.365, saturation:0.527, brightness:0.506, alpha:1)
+      
+      switch indexPath.row {
+      case 0:
+        configuration.image = Images.library
+      case 1:
+        configuration.image = Images.downloads
+      default:
+        configuration.image = Images.person
+      }
+      
+      cell.contentConfiguration = configuration
+      cell.backgroundConfiguration = UIBackgroundConfiguration.listSidebarCell()
+    })
+  }
+  
+  func configureDataSource() {
+    dataSource = UICollectionViewDiffableDataSource<Section, String>(collectionView: collectionView, cellProvider: { collectionView, indexPath, name -> UICollectionViewCell? in
+      return collectionView.dequeueConfiguredReusableCell(using: self.cellRegistration, for: indexPath, item: name)
+    })
+  }
+  
+  func updateData(with items: [String]) {
+    var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+    snapshot.appendSections([.main])
+    snapshot.appendItems(items)
+    DispatchQueue.main.async { self.dataSource.apply(snapshot, animatingDifferences: true) }
   }
 }
 
-extension RWItemsVC: UICollectionViewDataSource {
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return items.count
-  }
-  
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ItemCell", for: indexPath)
-    var contentConfiguration = UIListContentConfiguration.cell()
-    contentConfiguration.text = items[indexPath.row]
-    
-    if indexPath.row == 0 {
-      contentConfiguration.image = Images.library
-    } else if indexPath.row == 1 {
-      contentConfiguration.image = Images.downloads
-    } else {
-      contentConfiguration.image = Images.person
-    }
-    
-    cell.contentConfiguration = contentConfiguration
-    cell.backgroundConfiguration = UIBackgroundConfiguration.listSidebarCell()
-    cell.tintColor = UIColor(hue:0.365, saturation:0.527, brightness:0.506, alpha:1)
-    
-    return cell
-  }
-}
+//extension RWItemsVC: UICollectionViewDataSource {
+//  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//    return items.count
+//  }
+//
+//  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ItemCell", for: indexPath)
+//    var contentConfiguration = UIListContentConfiguration.cell()
+//    contentConfiguration.text = items[indexPath.row]
+//
+//    if indexPath.row == 0 {
+//      contentConfiguration.image = Images.library
+//    } else if indexPath.row == 1 {
+//      contentConfiguration.image = Images.downloads
+//    } else {
+//      contentConfiguration.image = Images.person
+//    }
+//
+//    cell.contentConfiguration = contentConfiguration
+//    cell.backgroundConfiguration = UIBackgroundConfiguration.listSidebarCell()
+//    cell.tintColor = UIColor(hue:0.365, saturation:0.527, brightness:0.506, alpha:1)
+//
+//    return cell
+//  }
+//}
 
 extension RWItemsVC: UICollectionViewDelegate {
   func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
--- Practical Example/RayWenderlich/Screens/RWMainVC.swift
+++ Practical Example/RayWenderlich/Screens/RWMainVC.swift
@@ -9,9 +9,9 @@ import UIKit
 
 class RWMainVC: UIViewController {
   
-  let windowInterfaceOrientation = SceneDelegate.windowInterfaceOrientation!
+  var tabBarVC = RWTabBarController()
+  var splitVC = RWSplitViewController(style: .tripleColumn)
   
-  var tabBarView = UIView()
   var splitView = UIView()
   
   override func viewDidLoad() {
@@ -19,32 +19,13 @@ class RWMainVC: UIViewController {
     
     configureViewController()
   }
-  
-  override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
-    super.willTransition(to: newCollection, with: coordinator)
-    
-    coordinator.animate(alongsideTransition: { [weak self] context in
-      guard let self = self else{ return }
-      
-      self.configureViewController()
-    })
-  }
+
   
   func configureViewController() {
-    guard let interfaceOrientation = SceneDelegate.windowInterfaceOrientation else { return }
-    
-    if interfaceOrientation.isPortrait {
-      self.splitView.removeFromSuperview()
-      self.view.addSubview(self.tabBarView)
-      self.tabBarView.pinToEdges(of: self.view)
-      
-      self.add(childVC: RWTabBarController(), to: self.tabBarView)
-    } else {
-      self.tabBarView.removeFromSuperview()
-      self.view.addSubview(self.splitView)
-      self.splitView.pinToEdges(of: self.view)
-      
-      self.add(childVC: RWSplitViewController(style: .tripleColumn), to: self.splitView)
-    }
+    view.addSubview(splitView)
+    splitView.pinToEdges(of: view)
+
+    self.add(childVC: splitVC, to: self.splitView)
+    splitVC.setViewController(tabBarVC, for: .compact)
   }
 }
