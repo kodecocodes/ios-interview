3d48eb4c2671d48210f4642790b6c5bfbc546b7c
Neil Hiddink <nhiddink2@gmail.com>
2020-08-17 20:08:13 -0400

    feat: Add App Icons

---
+++ Practical Example/RWArticles/RWArticles/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  RWArticles
+//
+//  Created by Neil Hiddink on 8/17/20.
+//  Copyright © 2020 Neil Hiddink. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
