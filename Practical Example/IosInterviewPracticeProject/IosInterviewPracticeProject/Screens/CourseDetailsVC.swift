7a667f94a420eaa91746eec0c1db54f9096796a9
Robert Ramirez <robertpramirez44@gmail.com>
2020-10-26 16:35:49 -0600

    Implemented a Dispatch Group for the loading of course content
    Added a Dispatch group to consolidate the loading of course content into a single process

--- Practical Example/IosInterviewPracticeProject/IosInterviewPracticeProject/Screens/CourseDetailsVC.swift
+++ Practical Example/IosInterviewPracticeProject/IosInterviewPracticeProject/Screens/CourseDetailsVC.swift
@@ -14,6 +14,7 @@ class CourseDetailsVC: UIViewController {
   @IBOutlet var courseTitle: UILabel!
   @IBOutlet var activityIndicator: UIActivityIndicatorView!
   @IBOutlet var courseBody: UITextView!
+  @IBOutlet var courseDescription: UILabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -22,44 +23,56 @@ class CourseDetailsVC: UIViewController {
 
   // MARK: - Load Course Content
   private func loadCourseContent() {
+    var description = ""
     var itemName = ""
+    var courseContent: NSMutableAttributedString?
+    var cardArtwork = UIImage()
+    let fetchCourseContentDispatchGroup = DispatchGroup()
     guard let courseContentUrl = courseContentUrl else { return }
     activityIndicator.startAnimating()
+    fetchCourseContentDispatchGroup.enter()
 
     CourseAPIService.shared.fetchCourseContent(for: courseContentUrl) { courseContentResults in
       switch courseContentResults {
       case .success(let courseContentResults):
         let item = courseContentResults.data
         DispatchQueue.main.async {
+          description = item.attributes.descriptionPlainText
           itemName = item.attributes.name
           let htmlContent = item.attributes.body ?? ""
           let htmlData = Data(htmlContent.utf8)
           let attributes = [NSAttributedString.Key.foregroundColor: UIColor.label, NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .body)]
 
-          if let courseContent = try? NSMutableAttributedString(data: htmlData, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
-            courseContent.addAttributes(attributes, range: _NSRange(location: 0, length: courseContent.length))
-            self.courseBody.attributedText = courseContent
+          if let body = try? NSMutableAttributedString(data: htmlData, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
+            courseContent?.addAttributes(attributes, range: _NSRange(location: 0, length: body.length))
+            courseContent = body
           }
+          fetchCourseContentDispatchGroup.leave()
         }
         guard let cardArtWorkUrl = URL(string: item.attributes.cardArtworkUrl) else { return }
-
+        fetchCourseContentDispatchGroup.enter()
         CourseAPIService.shared.fetchCardArtwork(for: cardArtWorkUrl) { results in
           switch results {
-          case .success(let cardArtWork):
-            DispatchQueue.main.async {
-              self.activityIndicator.stopAnimating()
-              self.courseLogoImageView.contentMode = .scaleAspectFill
-              self.courseLogoImageView.layer.cornerRadius = 5
-              self.courseLogoImageView.layer.masksToBounds = true
-              self.courseLogoImageView.image = cardArtWork
-              self.courseTitle.text = itemName
-            }
+          case .success(let artworkImage):
+            cardArtwork = artworkImage
+            fetchCourseContentDispatchGroup.leave()
           case .failure(let error):
             print("Failed to download content artwork: \(error)")
           }
         }
       case .failure(let error):
         print("Could not retrieve course content:\(error)")
+      }
+
+      fetchCourseContentDispatchGroup.notify(queue: .main) {
+        self.activityIndicator.stopAnimating()
+        self.courseDescription.text = description
+        self.courseBody.attributedText = courseContent
+        self.courseLogoImageView.contentMode = .scaleAspectFill
+        self.courseLogoImageView.layer.cornerRadius = 5
+        self.courseLogoImageView.layer.masksToBounds = true
+        self.courseLogoImageView.image = cardArtwork
+        self.courseTitle.text = itemName
       }
     }
   }
